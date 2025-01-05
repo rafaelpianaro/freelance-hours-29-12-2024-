4618e1555f9d828ec920e7500293a53b8d2cb461
Rafael Pianaro <rafaelpianaro@gmail.com>
2025-01-05 18:34:16 -0300

    - adicionando timer no projeto, "Encerra em:"

---
+++ resources/views/livewire/projects/timer.blade.php
@@ -0,0 +1,9 @@
+<div class="flex items-center justify-between mt-4 text-[14px]" wire:poll.1s>
+    <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
+    <div class="flex items-center space-x-1 font-bold">
+        <span class="text-white ">{{ $days }}</span><span>:</span>
+        <span class="text-white ">{{ $hours }}</span><span>:</span>
+        <span class="text-white ">{{ $minutes }}</span><span>:</span>
+        <span class="text-white ">{{ $seconds }}</span>
+    </div>
+</div>
